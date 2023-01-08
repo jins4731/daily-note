a3f7d34b374dbb34e28df7c18309384bddb76df9
jins4731 <jins4731@gmail.com>
2023-01-08 13:18:49 +0900

    nodejs app.js express 사용하여 변경

--- webDevComplete/nodejs/nodejs-demo/app.js
+++ webDevComplete/nodejs/nodejs-demo/app.js
@@ -1,14 +1,13 @@
-const http = require('http');
+const express = require('express');
 
-function handleRequest(request, response){
-    if(request.url === '/currenttime'){
-        response.statusCode = 200;
-        response.end('<h1>'+ new Date().toISOString() +'</h1>');
-    }else if(request.url === '/'){
-        response.statusCode = 200;
-        response.end('<h1>Hello World!</h1>');
-    }
-} 
-const server = http.createServer(handleRequest);
+const app = express();
 
-server.listen(3000); 
+app.get('/currenttime', function(req, res) {
+    res.send('<h1>'+ new Date().toISOString() +'</h1>');
+}) //localhost:3000/currenttime
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World!</h1>');
+}) //localhost:3000/
+
+app.listen(3000);
