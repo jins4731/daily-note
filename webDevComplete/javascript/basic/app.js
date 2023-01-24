2da53efb272c8abe3941b6b177b03a30c5725322
jins4731 <jins4731@gmail.com>
2023-01-24 15:33:34 +0900

    docs: webDevComplete - execrcise function

---
+++ webDevComplete/javascript/basic/app.js
@@ -0,0 +1,25 @@
+// let greetingText = "Hi, i\'M MAX!!!";       
+// let age = 32;
+// alert(greetingText);
+// greetingText = 'Hi, i am really MAX!';
+// alert(greetingText);
+// alert(age);
+
+let age = 32;
+let userName = "Max";
+let hobbies = ['Sports', 'cooking', 'Reading'];
+let job = {
+    title: 'Developer', 
+    place: 'New York',
+    salary: 50000
+};
+
+function calculateAdultYears(userAge){
+    return userAge-18;
+}
+
+let totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
+
+alert(hobbies[0]);
+alert(job.title);
