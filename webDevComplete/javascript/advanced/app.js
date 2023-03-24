376b2aabe0a16065e23560acd2b9845b5453301f
jins4731 <jins4731@gmail.com>
2023-03-24 22:43:01 +0900

    docs: WebDevComplete add newAnchorElement

--- webDevComplete/javascript/advanced/app.js
+++ webDevComplete/javascript/advanced/app.js
@@ -8,3 +8,18 @@ anchorElement.href='https://google.com';
 // anchorElement = document.querySelector('#external-id');
 anchorElement = document.querySelector('p a');
 anchorElement.href = 'https://academind.com';
+
+// ADD AN ELEMENT
+//1. Create the new element
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "This leads to Google";
+
+//2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector('p');
+
+//3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
